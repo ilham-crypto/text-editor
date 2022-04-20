9e0cb807751b96ff5dc028bdfd56cb06ee589066
ilham-crypto <ilhamsantoso1922@gmail.com>
2022-04-20 15:53:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React, { Component } from 'react';
-import { Editor } from 'react-draft-wysiwyg';
-import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+// import React, { Component } from 'react';
+// import { Editor } from 'react-draft-wysiwyg';
+// import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const EditorComponent = () => <Editor />
+// const EditorComponent = () => <Editor />
 
-export default EditorComponent;
+// export default EditorComponent;
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+function App() {
+  return (
+    <div>
+      <Router basename="/">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Add" component={Add} />
+          <Route path="/Edit/:postID" component={Edit} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Editor } from 'react-draft-wysiwyg';
-import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
-function uploadImageCallBack(file) {
-  return new Promise(
-    (resolve, reject) => {
-      const xhr = new XMLHttpRequest(); // eslint-disable-line no-undef
-      xhr.open('POST', 'https://api.imgur.com/3/image');
-      xhr.setRequestHeader('Authorization', 'Client-ID 8d26ccd12712fca');
-      const data = new FormData(); // eslint-disable-line no-undef
-      data.append('image', file);
-      xhr.send(data);
-      xhr.addEventListener('load', () => {
-        const response = JSON.parse(xhr.responseText);
-        resolve(response);
-      });
-      xhr.addEventListener('error', () => {
-        const error = JSON.parse(xhr.responseText);
-        reject(error);
-      });
-    },
-  );
-}
-
-const EditorImage = () => (
-  <Editor
-    wrapperClassName="demo-wrapper"
-    editorClassName="demo-editor"
-    toolbar={{
-      inline: { inDropdown: true },
-      list: { inDropdown: true },
-      textAlign: { inDropdown: true },
-      link: { inDropdown: true },
-      history: { inDropdown: true },
-      image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
-    }}
-  />
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <EditorImage/>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
